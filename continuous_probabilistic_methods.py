7823078ed2974bfcdca4e608773af573c4fb88b6
stephen-c-perry <stephen.christian.perry@gmail.com>
2023-02-21 16:57:36 -0600

    end of class save

--- continuous_probabilistic_methods.py
+++ continuous_probabilistic_methods.py
@@ -1,8 +1,40 @@
 import pandas as pd
 import numpy as np
 
-def get_lower_and_upper_bounds(series, multiplier = 1.5):
-    q1_range = round((len(series.index) * .25),0).astype(int)
-    q1 = series[:q1_range]
 
-    return q1
+def get_quartiles(series):
+    '''
+    Takes in a series (df.column) and returns 4 quartiles of 
+    that series
+    '''
+    q1_end = int(len(series) * .25)
+    q2_end = int(len(series) * .50)
+    q3_end = int(len(series) * .75)
+
+    q1 = series[:q1_end]
+    q2 = series[q1_end:q2_end]
+    q3 = series[q2_end:q3_end]
+    q4 = series[q3_end:]
+
+    return q1, q2, q3, q4
+
+
+
+
+
+def get_lower_and_upper_bounds(series, multiplier = 1.5):
+
+    q1_end = int(len(series) * .25)
+    q2_end = int(len(series) * .50)
+    q3_end = int(len(series) * .75)
+
+
+    q1 = series[:q1_end]
+    q2 = series[q1_end:q2_end]
+    q3 = series[q2_end:q3_end]
+    q4 = series[q3_end:]
+
+    IQR = series[q1_end:q3_end]
+
+
+    return q1, q2, q3, q4
